5128ddfeb0c3a1971f14da3bf7d5ceaaed52d756
Chris Vasselli <clindsay@gmail.com>
2017-09-16 14:38:27 -0400

    Initial Commit

---
+++ TestStatusBarBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestStatusBarBug
+//
+//  Created by Chris Vasselli on 9/16/17.
+//  Copyright © 2017 Chris Vasselli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
